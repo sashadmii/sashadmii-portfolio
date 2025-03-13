a2281e9c45123365e55e3aaca8728da68943a1f1
sashadmii <alexandra.rychka@gmail.com>
2025-03-12 22:27:35 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sashadmii.github.io',
+  base: '/sashadmii-portfolio',
   build: {
     outDir: './public/build',
   },
